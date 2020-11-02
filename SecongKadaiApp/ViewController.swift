4a50c9fd9732fb97b0aa4d3d2876da67e125543e
oymtty <oymtty.0526@gmail.com>
2020-11-02 12:57:49 +0900

    Initial Commit

---
+++ SecongKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecongKadaiApp
+//
+//  Created by Oyama Tetsuya on 2020/11/02.
+//  Copyright © 2020 Oyama Tetsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
